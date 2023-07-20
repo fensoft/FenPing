1b6ff067ee82d0e9b7618d00e8e84f6be216183b
fensoft <fensoft@gmail.com>
2023-07-20 09:03:49 +0000

    release 1.2

--- restart.sh
+++ restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-docker build -t fensoft/fenping:1.1 .
+VERSION=1.2
+docker build -t fensoft/fenping:$VERSION .
 . .env
 docker network rm lan
 docker network create -d macvlan --subnet=${NETWORK}.0/24 -o parent=${HOST_INTERFACE} lan
@@ -8,4 +9,4 @@ docker rm fenping
 if [ "$DEV" ]; then
   EXTRA="-v `pwd`:/var/www/html"
 fi
-docker run -d --name fenping --privileged --env-file .env -v `pwd`/data/dhcp:/var/lib/dhcp -v /sys/fs/cgroup:/sys/fs/cgroup:ro --sysctl net.ipv4.conf.all.send_redirects=0 --ip ${IP} -v `pwd`/data/db:/var/lib/mysql $EXTRA --net lan --restart unless-stopped -h fenping fensoft/fenping:1.1
+docker run -d --name fenping --privileged --env-file .env -v `pwd`/data/dhcp:/var/lib/dhcp -v /sys/fs/cgroup:/sys/fs/cgroup:ro --sysctl net.ipv4.conf.all.send_redirects=0 --ip ${IP} -v `pwd`/data/db:/var/lib/mysql $EXTRA --net lan --restart unless-stopped -h fenping fensoft/fenping:$VERSION
