cc07d609079d5b9354b1e1ac16995470c9627f88
fensoft <fensoft@gmail.com>
2026-07-08 01:09:55 +0000

    Reduce Docker container privileges

--- restart.sh
+++ restart.sh
@@ -9,5 +9,28 @@ docker rm fenping || true
 if [ "$DEV" ]; then
   EXTRA="-v `pwd`:/var/www/html"
 fi
-docker run -d --name fenping --privileged --env-file .env -e HOST_INTERFACE=end0 -e FENPING_NETWORK_MODE=host -v `pwd`/data/dnsmasq:/var/lib/misc -v `pwd`/data/nmap:/var/www/html/nmap -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v `pwd`/data/db:/var/lib/mysql $EXTRA --network host --restart unless-stopped -h fenping fensoft/fenping:$VERSION
+docker run -d \
+  --name fenping \
+  --cap-drop ALL \
+  --cap-add AUDIT_WRITE \
+  --cap-add CHOWN \
+  --cap-add DAC_OVERRIDE \
+  --cap-add FOWNER \
+  --cap-add SETGID \
+  --cap-add SETUID \
+  --cap-add NET_ADMIN \
+  --cap-add NET_BIND_SERVICE \
+  --cap-add NET_BROADCAST \
+  --cap-add NET_RAW \
+  --env-file .env \
+  -e HOST_INTERFACE=end0 \
+  -e FENPING_NETWORK_MODE=host \
+  -v `pwd`/data/dnsmasq:/var/lib/misc \
+  -v `pwd`/data/nmap:/var/www/html/nmap \
+  -v `pwd`/data/db:/var/lib/mysql \
+  $EXTRA \
+  --network host \
+  --restart unless-stopped \
+  -h fenping \
+  fensoft/fenping:$VERSION
  
